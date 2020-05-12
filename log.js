9ec3747ebff1e1dcabf02c53ac1db0b0589d448c
Sergey Nikitin <srg.post@gmail.com>
2020-05-12 12:04:17 +0700

    feat(lint): Apply eslint, reformat code

---
+++ log.js
@@ -0,0 +1,22 @@
+class Logger {
+	constructor(prefix) {
+		this.prefix = prefix;
+	}
+
+	writeOutput(message) {
+		// eslint-disable-next-line no-console
+		console.log(`[${this.prefix}]`, message);
+	}
+
+	writeWarning(message) {
+		// eslint-disable-next-line no-console
+		console.log(`[${this.prefix}]`, message);
+	}
+
+	writeError(message) {
+		// eslint-disable-next-line no-console
+		console.error(`[${this.prefix}]`, message);
+	}
+}
+
+module.exports = Logger;
