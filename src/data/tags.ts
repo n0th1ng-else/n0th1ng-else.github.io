87525b4188e4f41768c707cdab810b51864dc81b
Sergey Nikitin <srg.post@gmail.com>
2020-06-06 02:13:24 +0700

    feat(): Update tags

---
+++ src/data/tags.ts
@@ -0,0 +1,46 @@
+export const tags = [
+	{
+		title: 'Typescript',
+		url: 'https://www.typescriptlang.org'
+	},
+	{
+		title: 'Angular',
+		url: 'https://angular.io'
+	},
+	{
+		title: 'NodeJS',
+		url: 'https://nodejs.org'
+	},
+	{
+		title: 'Svelte',
+		url: 'https://svelte.dev'
+	},
+	{
+		title: 'React',
+		url: 'https://reactjs.org'
+	},
+	{
+		title: 'Redux',
+		url: 'https://redux.js.org'
+	},
+	{
+		title: 'Babel',
+		url: 'https://babeljs.io'
+	},
+	{
+		title: 'Prettier',
+		url: 'https://prettier.io'
+	},
+	{
+		title: 'ESLint',
+		url: 'https://eslint.org'
+	},
+	{
+		title: 'Jest',
+		url: 'https://jestjs.io'
+	},
+	{
+		title: 'Webpack',
+		url: 'https://webpack.js.org'
+	}
+];
