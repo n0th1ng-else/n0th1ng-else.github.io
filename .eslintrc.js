fb9643c805e7f6b0056400018d6593e02c6f71f2
Sergey Nikitin <srg.post@gmail.com>
2021-08-22 18:53:42 +0200

    fix(): Bump lint-staged. migrate to simple hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,31 @@
-const getCfg = () => {
-	return {
-		root: true,
-		parser: '@typescript-eslint/parser',
-		plugins: ['svelte3', '@typescript-eslint'],
-		overrides: [
-			{
-				files: ['*.svelte'],
-				processor: 'svelte3/svelte3'
-			}
-		],
-		env: {
-			node: true,
-			browser: true,
-			es6: true
-		},
-		globals: {
-			runtime: true
-		},
-		extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-		rules: {
-			'no-mixed-spaces-and-tabs': 'off',
-			'@typescript-eslint/no-var-requires': 'off',
-			'no-console': 'error'
-		},
-		settings: {
-			'svelte3/typescript': () => require('typescript'),
-			'svelte3/ignore-styles': () => true
+const getCfg = () => ({
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['svelte3', '@typescript-eslint'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3'
 		}
-	};
-};
+	],
+	env: {
+		node: true,
+		browser: true,
+		es6: true
+	},
+	globals: {
+		runtime: true
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	rules: {
+		'no-mixed-spaces-and-tabs': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'no-console': 'error'
+	},
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+		'svelte3/ignore-styles': () => true
+	}
+});
 
 module.exports = getCfg();
