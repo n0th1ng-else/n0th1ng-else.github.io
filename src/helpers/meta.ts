b420eaa5797f25bd5d93a4cde0c0358e06034dea
Sergey Nikitin <srg.post@gmail.com>
2021-09-26 00:00:02 +0200

    fix(): Fix tags

--- src/helpers/meta.ts
+++ src/helpers/meta.ts
@@ -1,3 +1,4 @@
 export type MetaType = 'website' | 'article' | 'blog';
 
-export type TwitterType = 'summary' | 'app' | 'gallery' | 'large version';
+// https://developer.twitter.com/en/docs/twitter-for-websites/cards/guides/getting-started
+export type TwitterType = 'summary' | 'summary_large_image' | 'app' | 'player';
