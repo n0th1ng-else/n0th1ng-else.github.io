4d4aba8a65314555e7877e9581d7076b7dce6c63
Sergey Nikitin <srg.post@gmail.com>
2023-10-15 22:17:35 +0200

    feat(deps): Bump dependencies

--- src/lib/server/markdown.ts
+++ src/lib/server/markdown.ts
@@ -1,21 +1,15 @@
-import { marked } from 'marked';
+import { Marked } from 'marked';
+import {markedHighlight} from "marked-highlight";
 import hljs from 'highlight.js';
 
-export const initMarkdown = (): void => {
-	marked.setOptions({
-		renderer: new marked.Renderer(),
-		highlight(code, lang) {
+export const renderMarkdown = async (md: string): Promise<string> => {
+	const parser = new Marked(markedHighlight({
+		langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+		highlight: (code, lang) => {
 			const language = hljs.getLanguage(lang) ? lang : 'plaintext';
 			return hljs.highlight(code, { language }).value;
 		},
-		langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
-		pedantic: false,
-		gfm: true,
-		breaks: false,
-		sanitize: false,
-		smartypants: false,
-		xhtml: false
-	});
-};
+	}))
 
-export const renderMarkdown = (md: string): string => marked(md);
+	return parser.parse(md);
+}
--- src/routes/api/v1/markdown/+server.ts
+++ src/routes/api/v1/markdown/+server.ts
@@ -1,14 +1,13 @@
 import { error, json } from '@sveltejs/kit';
-import { initMarkdown, renderMarkdown } from '$lib/server/markdown';
+import { renderMarkdown } from '$lib/server/markdown';
 import { Logger } from '$lib/common/log';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const logger = new Logger('api:markdown');
-	initMarkdown();
 	try {
 		const md = await request.text();
-		const result = md ? renderMarkdown(md) : '';
+		const result = md ? await renderMarkdown(md) : '';
 
 		return json({ result });
 	} catch (err) {
